3a60f8978e9ab3ed580784712135c67c9a60adc2
Philly Tan <phillberttan@gmail.com>
2017-12-14 15:45:45 +0800

    Bug Fix

--- src.php
+++ src.php
@@ -4,12 +4,12 @@ namespace NewsAPI;
 
 class NewsAPI {
     protected $APIKey, $category, $language, $country;
-    private $cu;
     public function __construct($APIKey = null) {
         $this->APIKey = $APIKey;
     }
     public function setAPIKey($APIKey) {
         $this->APIKey = $APIKey;
+        return $this;
     }
     public function category($category) {
         $this->category = $category;
@@ -23,20 +23,34 @@ class NewsAPI {
         $this->country = $country;
         return $this;
     }
-    public function sources() {
-        $this->cu = curl_init();
+    public function sources($arr = null) {
+        if ($arr != null) {
+            $body = $arr;
+        }
+        else {
+            $body = [
+                'country' => $this->country,
+                'language' => $this->language,
+                'category' => $this->category
+            ];
+        }
+        $url = 'https://newsapi.org/v2/sources' . "?" . http_build_query($body);
+        return $this->sendRequest($url);
+    }
+    private function sendRequest($url) {
+        $cu = curl_init();
         $curlArgs = [
-            CURLOPT_URL => 'https://newsapi.org/v2/sources',
-            CURLOPT_HTTPHEADER => array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => [
                 "Authorization: ".$this->APIKey
-            ),
+            ],
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_SSL_VERIFYPEER => 0,
             CURLOPT_SSL_VERIFYHOST => 0
         ];
-        curl_setopt_array($this->cu, $curlArgs);
-        return curl_exec($this->cu);
+        curl_setopt_array($cu, $curlArgs);
+        return curl_exec($cu);
     }
 }
 
